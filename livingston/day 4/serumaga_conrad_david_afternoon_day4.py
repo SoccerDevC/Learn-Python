c814a0f358d35754e028d4fd92d33b594ccb6778
conrad-coder <cddavid2001@gmail.com>
2023-06-26 15:27:16 +0300

    Basics of functions in python

---
+++ livingston/day 4/serumaga_conrad_david_afternoon_day4.py
@@ -0,0 +1,39 @@
+# #Group blocks of code
+# #There is need for code that is clean, reusable, maintainable
+
+# #Declaring a function
+# #def function_name():
+
+# def afternoon():
+#     name = input("What is your name: ")
+#     print("Good afternoon!", name)
+#     print("Class starts at 2pm")
+
+# def driver(name="Boy"):
+#     print("Hello", name)
+
+# def age(x = 20):
+#     return x
+
+# def add(x, y):
+#     return x + y
+
+# def subtract(y,x = 6):
+#     return x - y
+
+# #Calling a function
+# afternoon()
+
+#Function parameters and arguments
+#Arguments are specified after the function name
+#parameters are the
+
+#A simple calc
+def add(s,w):
+    return(s + w)
+
+def product(s,w):
+    return(s * w)
+
+import serumaga_conrad_david_afternoon_day4
+print(serumaga_conrad_david_afternoon_day4.product(8,4))
